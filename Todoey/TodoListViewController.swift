c531dff96f8490756a69b54a03efc65e07878461
Scott Wachtler <scottwachtler@scotts-mbp.roc.mn.charter.com>
2018-06-09 18:16:48 -0500

    Preliminary Code. First Draft!

---
+++ Todoey/TodoListViewController.swift
@@ -0,0 +1,56 @@
+//
+//  ViewController.swift
+//  Todoey
+//
+//  Created by Scott Wachtler on 6/9/18.
+//  Copyright © 2018 Scott Wachtler. All rights reserved.
+//
+
+import UIKit
+
+class TodoListViewController: UITableViewController, UITextViewDelegate {
+
+    let itemArray = ["Find Mike", "Find Stue", "Fly Away"]
+        
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    //MARK - Tableview Datasource Methods
+    
+    //Cell Display, and how many rows.
+    
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        
+        let cell = tableView.dequeueReusableCell(withIdentifier: "ToDoItemCell", for: indexPath)
+        
+        cell.textLabel?.text = itemArray [indexPath.row]
+        
+        return cell
+    }
+    
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        //code
+       return  itemArray.count
+    }
+
+    
+    //MARK - TableView Delegate Methods
+    
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+       // print(itemArray[indexPath.row])
+        if tableView.cellForRow(at: indexPath)?.accessoryType == .checkmark {
+            tableView.cellForRow(at: indexPath)?.accessoryType = .none
+        } else {
+        tableView.cellForRow(at: indexPath)?.accessoryType = .checkmark
+        }
+        tableView.deselectRow(at: indexPath , animated: true)
+        
+    }
+    
+
+}
+
